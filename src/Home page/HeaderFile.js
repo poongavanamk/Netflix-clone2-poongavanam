a9f5a0d058153471717a1491a835e8907ea6ca16
poongavanamk <punga8599@gmail.com>
2025-05-15 14:12:00 +0530

    Your second clone

--- src/Home page/HeaderFile.js
+++ src/Home page/HeaderFile.js
@@ -1,19 +1,19 @@
-import React from 'react'
 import img from '../Mainfolder/netflix.jpg'
 import MovieList from './MovieList'
 import { Link } from 'react-router-dom'
+import style from './Header.module.css'
 
 
 
 export default function HeaderFile() {
   return (
     <div>
-      <div className='tophead'>
-          <div className='header'>
+      <div className={style.tophead}>
+          <div className={style.header}>
              <img src={img} alt='netflix '></img>
              <p>UNLIMITED TV SHOWS & MOVIES</p>
-             <Link to="/signup"><button className='joinnow'>Join Now</button></Link>
-              <Link to="/loginpage"><button className='signin'>SignIN</button></Link>
+             <Link to="/signup"><button className={style.joinnow}>Join Now</button></Link>
+              <Link to="/loginpage"><button className={style.signin}>SignIN</button></Link>
           </div>
        </div>
        <MovieList />
